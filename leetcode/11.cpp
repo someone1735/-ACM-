17948ebc1407f982afeae8af581e34bd9680e3dc
restart <1735453850@qq.com>
2026-03-28 10:43:09 +0800

    c++

---
+++ leetcode/11.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int l=0,r=n-1;
+        int ans=-1;
+        while(l<r)
+        {
+            ans=max(min(height[l],height[r])*(r-l),ans);
+            if(height[l]<height[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+};
